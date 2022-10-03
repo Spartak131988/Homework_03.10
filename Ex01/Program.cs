2c8f2b1fdd04a539c34da0faa9d60b38a6a5fb82
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-10-03 12:07:58 +0700

    Программа, которая покажет количество чётных чисел в массиве

---
+++ Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] Create(int len)
+{
+    return new int[len];
+}
+
+void Fill(int[] array, int min, int max)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+string Print(int[] array)
+{
+    return "[" + String.Join(',', array) + "]";
+}
+
+int SearchEvenNumbers(int[] array)
+{
+    int pos = 0;
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            pos ++;
+        }
+    }
+    return pos;
+}
+
+
+int[] arr = Create(5);
+Fill(arr, 100, 1000);
+Console.WriteLine("Массив: " + Print(arr));
+Console.WriteLine("Количество чётных чисел в массиве: " + SearchEvenNumbers(arr));
