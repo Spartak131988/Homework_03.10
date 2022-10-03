f50cd212e1bc13e31c3cf6c05ae6efd2ab77b53f
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-10-03 12:13:25 +0700

    Сумма элементов, стоящих на нечётных позициях

---
+++ Ex02/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] Create(int len)
+{
+    return new int[len];
+}
+
+void Fill(int[] array, int min, int max)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+string Print(int[] array)
+{
+    return "[" + String.Join(',', array) + "]";
+}
+
+int SumOddIndex(int[] array)
+{
+    int sum = 0;
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int[] coll = Create(5);
+Fill(coll, -100, 100);
+Console.WriteLine("Массив: " + Print(coll));
+Console.WriteLine("Сумму элементов, стоящих на нечётных позициях: " + SumOddIndex(coll));
